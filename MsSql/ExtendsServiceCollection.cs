c0a5f29d368b9a41476ffba9b3714bc19a434015
James Woodley <james@lightestnight.com>
2020-05-17 14:17:29 +0100

    Initial Commit

---
+++ MsSql/ExtendsServiceCollection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using SqlStreamStore;
+
+namespace LightestNight.System.EventSourcing.SqlStreamStore.MSSql
+{
+    public static class ExtendsServiceCollection
+    {
+        public static IServiceCollection AddMsSqlEventStore(this IServiceCollection services,
+            Action<MsSqlEventSourcingOptions>? optionsAccessor = null, params Assembly[] eventAssemblies)
+        {
+            var msSqlOptions = new MsSqlEventSourcingOptions();
+            optionsAccessor?.Invoke(msSqlOptions);
+            // ReSharper disable once RedundantAssignment
+            services.AddEventStore(eventSourcingOptions => eventSourcingOptions = msSqlOptions, eventAssemblies);
+
+            services.Configure(optionsAccessor);
+            
+            var serviceProvider = services.BuildServiceProvider();
+            if (!(serviceProvider.GetService<IStreamStore>() is MsSqlStreamStore))
+            {
+                services.AddSingleton<IStreamStore>(sp =>
+                {
+                    var streamStore = new MsSqlStreamStore(new MsSqlStreamStoreSettings(msSqlOptions.ConnectionString)
+                    {
+                        Schema = msSqlOptions.Schema
+                    });
+
+                    if (msSqlOptions.CreateSchemaIfNotExists)
+                        Task.Run(async () => await streamStore.CreateSchema().ConfigureAwait(false));
+
+                    return streamStore;
+                });
+            }
+
+            return services;
+        }
+    }
+}
---
+++ MsSql/MsSqlEventSourcingOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace LightestNight.System.EventSourcing.SqlStreamStore.MSSql
+{
+    public class MsSqlEventSourcingOptions : EventSourcingOptions
+    {
+        /// <summary>
+        /// Whether to create the database schema if it doesn't already exist
+        /// </summary>
+        public bool CreateSchemaIfNotExists { get; set; } = true;
+
+        /// <summary>
+        /// The SQL Server Connection String
+        /// </summary>
+        public string ConnectionString { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The Microsoft Sql Server database schema to use
+        /// </summary>
+        public string Schema { get; set; } = "dbo";
+    }
+}
