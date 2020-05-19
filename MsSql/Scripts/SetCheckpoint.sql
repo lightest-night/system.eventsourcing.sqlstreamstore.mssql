899abc4114db400fbf3206f7faead3521e93fb6b
James Woodley <james@lightestnight.com>
2020-05-19 15:06:13 +0100

    Use an UPSERT for Setting Checkpoint

--- MsSql/Scripts/SetCheckpoint.sql
+++ MsSql/Scripts/SetCheckpoint.sql
@@ -1,2 +1,12 @@
-﻿INSERT INTO [dbo].[Checkpoints] ([CheckpointId], [Checkpoint])
-VALUES (@CheckpointId, @Checkpoint)
+﻿SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+BEGIN TRAN
+    
+    IF EXISTS ( SELECT [CheckpointId] FROM [dbo].[Checkpoints] WITH (UPDLOCK) WHERE [CheckpointId] = @CheckpointId)
+        UPDATE [dbo].[Checkpoints]
+        SET [Checkpoint] = @Checkpoint
+        WHERE [CheckpointId] = @CheckpointId;
+    ELSE
+        INSERT INTO [dbo].[Checkpoints] ([CheckpointId], [Checkpoint])
+        VALUES (@CheckpointId, @Checkpoint);
+        
+COMMIT
